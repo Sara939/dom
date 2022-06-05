f7e9b25341e2410ce3312f6e866568126fca38e9
sara-likaount <saralikaunt@gmail.com>
2022-06-05 21:04:24 +0300

    m

---
+++ main.js
@@ -0,0 +1,34 @@
+// 1:
+// console.log (first.innerText);
+// 2://///////////////////////////////////////////////////
+// var newt= document.getElementsByTagName("p");
+// newt.innerHTML= "my dom app";
+// 3:
+// console.log(document.getElementsByTagName("h2"));
+// console.log(document.getElementsByTagName("div"));
+// console.log(document.getElementsByTagName("h3"));
+// console.log(document.getElementsByTagName("span"));
+// // 4:
+// console.log(document.getElementsByTagName("div"));
+// 5:
+// textsave= document.getElementsByTagName("h3");
+// console.log(textsave.innerText);
+
+
+
+
+
+
+
+
+
+// // 12:
+// function addspan(){
+//     var idont= document.getElementsByTagName("p");
+
+//     // for(){
+//     idont.innerHTML="<span>uuuuu</span>";
+//     // }
+// }
+
+// addspan();
