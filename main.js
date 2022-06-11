2061efb5df95985f19361c0654f57df05ee62dda
sara-likaount <saralikaunt@gmail.com>
2022-06-11 18:04:38 +0300

    m

--- main.js
+++ main.js
@@ -13,11 +13,40 @@
 // 5:
 // textsave= document.getElementsByTagName("h3");
 // console.log(textsave.innerText);
+// 6:
+// for(var i=0; i<4; i++){
+//     var textsave=0;
+//     textsave= document.getElementsByTagName("p")[i].innerHTML;
+//     console.log(textsave);
+// }
+// 7:
+// function getname(){
+//     document.getElementById("ma").innerText = prompt("name");
+    
+// }
+// getname();
+// 8:
+// function getfromuser(){
+//     var num= +prompt("num"); 
+//     var mycolor= document.getElementById("ma").innerText = prompt("color");
 
+//     for(var i=0; i<num-1; i++){
+        
+//         document.write(mycolor);
+//     }
+// }
 
+// getfromuser();
+// 9:
+function builddom(){
+    var txt = prompt("text"); 
+    var tagname = prompt ("tagname");
+    document.createElement("tagname");
+    tagname.innerText = txt;
+   
+}
 
-
-
+builddom();
 
 
 
